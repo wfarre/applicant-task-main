df9540fc3fc7397d404a8cdd670bfda0db8796dd
wfarre <william.farre@gmail.com>
2024-08-29 17:06:27 +0800

    fix: header size mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 export const Header = () => {
   return (
     <header className="mb-8 w-full bg-zinc-900 pb-6 pt-6 text-white">
-      <h1 className="text-center text-5xl font-semibold uppercase">Results</h1>
+      <h1 className="text-center text-4xl font-semibold uppercase md:text-5xl">
+        Results
+      </h1>
     </header>
   );
 };

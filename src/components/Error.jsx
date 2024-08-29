660bba2177250b4d951cf4c2931f2e48b6096b93
wfarre <william.farre@gmail.com>
2024-08-29 16:56:37 +0800

    fix: image not displayed

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,19 +1,15 @@
 import PropTypes from "prop-types";
-const errorImg = lazy(() => import("../assets/images/undraw_server_down.svg"));
-
-import { lazy, Suspense } from "react";
+import errorImg from "../assets/images/undraw_server_down.svg";
 
 const Error = ({ errorMsg }) => {
   return (
     <section className="mx-6 my-6 md:mx-36">
       <div className="relative mx-auto aspect-[1119/699] w-full max-w-96">
-        <Suspense>
-          <img
-            className="absolute left-0 top-0 h-full w-full"
-            src={errorImg}
-            alt=""
-          />
-        </Suspense>
+        <img
+          className="absolute left-0 top-0 h-full w-full"
+          src={errorImg}
+          alt=""
+        />
       </div>
       <p className="mt-6 text-center text-xl">{errorMsg}</p>
     </section>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,14 @@
-import { lazy, Suspense } from "react";
-const logo = lazy(() => import("../assets/images/xterra-logo.svg"));
+import logo from "../assets/images/xterra-logo.svg";
 
 const Footer = () => {
   return (
     <footer className="absolute bottom-0 left-0 w-full bg-zinc-900 py-6">
       <div className="relative mx-auto h-4">
-        <Suspense>
-          <img
-            className="absolute left-0 top-0 h-full w-full"
-            src={logo}
-            alt="logo xterra"
-          />
-        </Suspense>
+        <img
+          className="absolute left-0 top-0 h-full w-full"
+          src={logo}
+          alt="logo xterra"
+        />
       </div>
     </footer>
   );
